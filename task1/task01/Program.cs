136cfee361146839c70967037f1aa7080ad8bbac
Наталья Ярцева <zabaluna@mail.ru>
2022-10-16 00:37:10 +0300

    sqrtNumber

---
+++ task1/task01/Program.cs
@@ -0,0 +1,12 @@
+﻿// // 1 Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+ Console.Write("Введите число: ");
+
+ int number = Convert.ToInt32(Console.ReadLine());
+
+ int sqrt = number * number;
+// // number *= number;
+// // number = number * number;
+ int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+ Console.WriteLine("Число в квадрате: " + sqrt1);
