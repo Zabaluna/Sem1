b72bbbd281ce3506b9445395948a2363c922df95
Наталья Ярцева <zabaluna@mail.ru>
2022-10-16 01:26:15 +0300

    numerA*namberA==numberB

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая на вход принимает 2 числа и проверяет, является ли первое число квадратом второго
+
+Console.WriteLine("Введите два числа: "); // ввести подряд два числа с новой строчки 
+
+int numberA = Convert.ToInt32(Console.ReadLine()); // число один
+
+int numberB = Convert.ToInt32(Console.ReadLine()); // число два
+
+
+if (numberA*numberA == numberB)  
+{
+    Console.Write("Второе число является квадратом первого: " );
+}
+else
+{
+    Console.Write("Второе число НЕ является квадратом первого: " );
+}
+
