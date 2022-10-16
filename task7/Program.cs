dec22412cf1b89affb2b6e118d3e5e7341796239
Наталья Ярцева <zabaluna@mail.ru>
2022-10-17 00:43:37 +0300

    Max of three numbers

---
+++ task7/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа: "); // ввести подряд два числа с новой строчки 
+
+int a = Convert.ToInt32(Console.ReadLine()); // число один
+
+int b = Convert.ToInt32(Console.ReadLine()); // число два
+
+int c = Convert.ToInt32(Console.ReadLine()); // число три
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное число: " + max);
+
